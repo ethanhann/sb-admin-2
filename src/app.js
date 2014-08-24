d23596e1e3bd5f5243d1889e07c7e920b81aaf69
Ethan Hann <ethanhann@gmail.com>
2014-08-24 15:48:34 -0400

    Add 'authenticated' router state to distinguish auth/unauthed

--- src/app.js
+++ src/app.js
@@ -7,53 +7,57 @@ function ($stateProvider, $urlRouterProvider) {
             abstract: true,
             templateUrl: 'app.html'
         })
-        .state('sbAdmin2.blank', {
+        .state('sbAdmin2.authenticated', {
+            abstract: true,
+            templateUrl: 'authenticated/authenticated.html'
+        })
+        .state('sbAdmin2.authenticated.blank', {
             url: '/blank',
             templateUrl: 'blank/blank.html'
         })
-        .state('sbAdmin2.buttons', {
+        .state('sbAdmin2.authenticated.buttons', {
             url: '/buttons',
             templateUrl: 'buttons/buttons.html'
         })
-        .state('sbAdmin2.dashboard', {
+        .state('sbAdmin2.authenticated.dashboard', {
             url: '/',
             templateUrl: 'dashboard/dashboard.html'
         })
-        .state('sbAdmin2.flot', {
+        .state('sbAdmin2.authenticated.flot', {
             url: '/flot',
             templateUrl: 'flot/flot.html'
         })
-        .state('sbAdmin2.forms', {
+        .state('sbAdmin2.authenticated.forms', {
             url: '/forms',
             templateUrl: 'forms/forms.html'
         })
-        .state('sbAdmin2.grid', {
+        .state('sbAdmin2.authenticated.grid', {
             url: '/grid',
             templateUrl: 'grid/grid.html'
         })
-        .state('sbAdmin2.login', {
-            url: '/login',
-            templateUrl: 'login/login.html'
-        })
-        .state('sbAdmin2.morris', {
+        .state('sbAdmin2.authenticated.morris', {
             url: '/morris',
             templateUrl: 'morris/morris.html',
             controller: 'MorrisCtrl'
         })
-        .state('sbAdmin2.notifications', {
+        .state('sbAdmin2.authenticated.notifications', {
             url: '/notifications',
             templateUrl: 'notifications/notifications.html'
         })
-        .state('sbAdmin2.panels-wells', {
+        .state('sbAdmin2.authenticated.panels-wells', {
             url: '/panels-wells',
             templateUrl: 'panels-wells/panels-wells.html'
         })
-        .state('sbAdmin2.tables', {
+        .state('sbAdmin2.authenticated.tables', {
             url: '/tables',
             templateUrl: 'tables/tables.html'
         })
-        .state('sbAdmin2.typography', {
+        .state('sbAdmin2.authenticated.typography', {
             url: '/typography',
             templateUrl: 'typography/typography.html'
+        })
+        .state('sbAdmin2.login', {
+            url: '/login',
+            templateUrl: 'login/login.html'
         });
 }]);
