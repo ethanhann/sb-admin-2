54742aa4a3a8b90dd918960fc18e9e9ff1b11b81
Ethan Hann <ethanhann@gmail.com>
2014-08-24 14:09:19 -0400

    Begin converting demo to angular app

---
+++ src/app.js
@@ -0,0 +1,58 @@
+angular.module('sbAdmin2', ['ui.router'])
+.config(['$stateProvider', '$urlRouterProvider',
+function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('sbAdmin2', {
+            abstract: true,
+            templateUrl: 'app.html'
+        })
+        .state('sbAdmin2.index', {
+            url: '/',
+            templateUrl: 'index/index.html'
+        })
+        .state('sbAdmin2.blank', {
+            url: '/blank',
+            templateUrl: 'blank/blank.html'
+        })
+        .state('sbAdmin2.buttons', {
+            url: '/buttons',
+            templateUrl: 'buttons/buttons.html'
+        })
+        .state('sbAdmin2.flot', {
+            url: '/flot',
+            templateUrl: 'flot/flot.html'
+        })
+        .state('sbAdmin2.forms', {
+            url: '/forms',
+            templateUrl: 'forms/forms.html'
+        })
+        .state('sbAdmin2.grid', {
+            url: '/grid',
+            templateUrl: 'grid/grid.html'
+        })
+        .state('sbAdmin2.login', {
+            url: '/login',
+            templateUrl: 'login/login.html'
+        })
+        .state('sbAdmin2.morris', {
+            url: '/morris',
+            templateUrl: 'morris/morris.html'
+        })
+        .state('sbAdmin2.notifications', {
+            url: '/notifications',
+            templateUrl: 'notifications/notifications.html'
+        })
+        .state('sbAdmin2.panels-wells', {
+            url: '/panels-wells',
+            templateUrl: 'panels-wells/panels-wells.html'
+        })
+        .state('sbAdmin2.tables', {
+            url: '/tables',
+            templateUrl: 'tables/tables.html'
+        })
+        .state('sbAdmin2.typography', {
+            url: '/typography',
+            templateUrl: 'typography/typography.html'
+        });
+}]);
--- src/js/sb-admin-2.js
+++ src/js/sb-admin-2.js
@@ -1,14 +1,12 @@
-$(function() {
-
+$(function () {
     $('#side-menu').metisMenu();
-
 });
 
 //Loads the correct sidebar on window load,
 //collapses the sidebar on window resize.
 // Sets the min-height of #page-wrapper to window size
-$(function() {
-    $(window).bind("load resize", function() {
+$(function () {
+    $(window).bind("load resize", function () {
         topOffset = 50;
         width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
         if (width < 768) {
